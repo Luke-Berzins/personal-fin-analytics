88d96e0cc2f1770aaa1af9a2a5ffe0c11acc3cf1
Luke-Berzins <lukeberzins16@gmail.com>
2023-11-26 17:31:06 -0500

    db fetch queries configured

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,4 @@
-import { sql } from '@vercel/postgres';
+import { sql } from '@vercel/postgres'
 import {
   CustomerField,
   CustomersTable,
@@ -19,7 +19,7 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
